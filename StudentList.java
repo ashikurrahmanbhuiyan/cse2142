7ada8f4873d606a78537bcacbb7b6f3cd08ec7bf
ashikurrahmanbhuiyan <s2010376107@ru.ac.bd>
2022-11-30 11:05:38 +0600

    step #5 done

--- StudentList.java
+++ StudentList.java
@@ -7,70 +7,70 @@ public class StudentList {
 
 		// Check arguments
 		if(args.length == 0){
-			System.out.println("put a valid argument");
+			System.out.println(constants.errorMessage);
 		}else{
-		if (args[0].equals("a")) {
-			System.out.println("Loading data ...");
+		if (args[0].equals(constants.displayCommand)) {
+			System.out.println(constants.waitingMessage);
 			try {
 				String read = students();
-				String student[] = read.split(", ");
+				String student[] = read.split(constants.separator);
 				for (String names : student) {
 					System.out.println(names);
 				}
 			} catch (Exception e) {
 			}
-			System.out.println("Data Loaded.");
-		} else if (args[0].equals("r")) {
-			System.out.println("Loading data ...");
+			System.out.println(constants.endMessage);
+		} else if (args[0].equals(constants.randomAccessCommand)) {
+			System.out.println(constants.waitingMessage);
 			try {
 				String read = students();
-				String student[] = read.split(", ");
+				String student[] = read.split(constants.separator);
 				Random pick = new Random();
 				int picked = pick.nextInt(student.length);
 				System.out.println(student[picked]);
 			} catch (Exception e) {
 			}
-			System.out.println("Data Loaded.");
-		} else if (args[0].contains("+")) {
-			System.out.println("Loading data ...");
+			System.out.println(constants.endMessage);
+		} else if (args[0].contains(constants.addCommand)) {
+			System.out.println(constants.waitingMessage);
 			try {
 				BufferedWriter students = student();
-				String t = args[0].substring(1);
+				String t = args[0].substring(constants.one);
 				Date date = new Date();
-				String format= "dd/mm/yyyy-hh:mm:ss a";
+				String format= constants.timeFormat;
 				DateFormat dateFormat = new SimpleDateFormat(format);
 				String dates = dateFormat.format(date);
-				students.write(", " + t + "\nList last updated on " + dates);
+				students.write(constants.separator + t + constants.updateMessage + dates);
 				students.close();
 			} catch (Exception e) {
 			}
 
-			System.out.println("Data Loaded.");
-		} else if (args[0].contains("?")) {
-			System.out.println("Loading data ...");
+			System.out.println(constants.endMessage);
+		} else if (args[0].contains(constants.searchCommand)) {
+			System.out.println(constants.waitingMessage);
 			try {
 				String read = students();
-				String student[] = read.split(", ");
+				String student[] = read.split(constants.separator);
 				boolean done = false;
-				String t = args[0].substring(1);
+				String t = args[0].substring(constants.one);
 				for (int idx = 0; idx < student.length && !done; idx++) {
 					if (student[idx].equals(t)) {
-						System.out.println("We found it!");
+						System.out.println(constants.foundMessage);
 						done = true;
 					}
 				}
 			} catch (Exception e) {
 			}
-			System.out.println("Data Loaded.");
-		} else if (args[0].contains("c")) {
-			System.out.println("Loading data ...");
+			System.out.println(constants.endMessage);
+		} else if (args[0].contains(constants.countCommand)) {
+			System.out.println(constants.waitingMessage);
 			try {
 				String read = students();
 				char letters[] = read.toCharArray();
 				boolean in_word = false;
 				int count = 0;
 				for (char letter : letters) {
-					if (letter == ' ') {
+					if (letter == constants.singleSpaceChar) {
 						if (!in_word) {
 							count++;
 							in_word = true;
@@ -79,26 +79,26 @@ public class StudentList {
 						}
 					}
 				}
-				System.out.println(count + " word(s) found ");
+				System.out.println(count + constants.countMessage);
 			} catch (Exception e) {
 			}
-			System.out.println("Data Loaded.");
+			System.out.println(constants.endMessage);
 		}else{
-			System.out.println("put a valid argument");
+			System.out.println(constants.errorMessage);
 		}
 	}
 	}
 
 	private static BufferedWriter student() throws IOException {
 		BufferedWriter students = new BufferedWriter(
-				new FileWriter("students.txt", true));
+				new FileWriter(constants.inputFileName, true));
 		return students;
 	}
 
 	private static String students() throws FileNotFoundException, IOException {
 		BufferedReader students = new BufferedReader(
 				new InputStreamReader(
-						new FileInputStream("students.txt")));
+						new FileInputStream(constants.inputFileName)));
 		String read = students.readLine();
 		students.close();
 		return read;
